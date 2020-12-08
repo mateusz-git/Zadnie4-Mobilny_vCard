d696e4834ccf852395dab5334fc502d10fa0d8e9
mateusz-git <215851@edu.p.lodz.pl>
2020-12-08 14:26:27 +0100

    Generated project

---
+++ src/main/java/com/example/Zadnie4Mobilny_vCard/Zadnie4MobilnyVCardApplication.java
@@ -0,0 +1,13 @@
+package com.example.Zadnie4Mobilny_vCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadnie4MobilnyVCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zadnie4MobilnyVCardApplication.class, args);
+	}
+
+}
