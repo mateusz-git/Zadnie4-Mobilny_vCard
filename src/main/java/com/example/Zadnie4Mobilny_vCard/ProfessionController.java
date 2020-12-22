e3686275653939d71ad480b12c00416d16286472
mateusz-git <215851@edu.p.lodz.pl>
2020-12-22 14:49:02 +0100

    Renamed Controller  and change from @RestController to @Controller

--- src/main/java/com/example/Zadnie4Mobilny_vCard/ProfessionController.java
+++ src/main/java/com/example/Zadnie4Mobilny_vCard/ProfessionController.java
@@ -7,9 +7,9 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.io.*;
 import java.net.URL;
@@ -19,8 +19,8 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
-public class Controller {
+@Controller
+public class ProfessionController {
     @GetMapping("/getvCard/{k}/{email}")
     public ResponseEntity getvCard(@PathVariable String k, @PathVariable String email) throws IOException {
         String url = "https://panoramafirm.pl/szukaj?k=" + k + "&l=";
